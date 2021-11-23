67edf54f6ae0016513d9b37946e584b4425040c5
Andrey Filippov <viper4478@gmail.com>
2021-11-23 07:40:20 +0300

    refactor code

--- src/main/java/ru/innotech/education/rxjava/AppendOnlyList.java
+++ src/main/java/ru/innotech/education/rxjava/AppendOnlyList.java
@@ -1,33 +1,29 @@
 package ru.innotech.education.rxjava;
 
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class AppendOnlyList<T> extends AbstractCollection<T>
         implements Collection<T> {
     private final List<T> elementData;
     private final List<Boolean> listOfDelete;
-    private int countList;
+    private final AtomicInteger countList = new AtomicInteger(0);
+    private final Object sync = new Object();
 
     public AppendOnlyList() {
-        elementData = new ArrayList<>();
-        listOfDelete = new ArrayList<>();
+        this.elementData = new ArrayList<>();
+        this.listOfDelete = new ArrayList<>();
     }
 
-    public AppendOnlyList(Collection<T> c) {
+    public AppendOnlyList(final Collection<T> c) {
         this();
         addAll(c);
     }
 
     @Override
     public int size() {
-        /*countList = 0;
-        for (Boolean i : listOfDelete) {
-            if (!i) {
-                countList++;
-            }
-        }*/
-        return countList;
+        return countList.get();
     }
 
     @Override
@@ -36,59 +32,61 @@ public class AppendOnlyList<T> extends AbstractCollection<T>
     }
 
     @Override
-    public boolean contains(Object o) {
+    public boolean contains(final Object o) {
         for (int i = 0; i < elementData.size(); i++) {
-            if (elementData.get(i).equals(o) && !listOfDelete.get(i)) return true;
+            if (elementData.get(i).equals(o) && !listOfDelete.get(i))
+                return true;
         }
         return false;
     }
 
     @Override
-    public synchronized Iterator<T> iterator() {
-        /*List<T> tempList = new ArrayList<>();
-
-        for (T temp : elementData) {
-            if (contains(temp))
-                tempList.add(temp);
-        }*/
-        return elementData.stream().filter(this::contains).collect(Collectors.toList()).iterator();
-                //tempList.iterator();
+    public Iterator<T> iterator() {
+        return new Itr();
     }
 
     @Override
     public Object[] toArray() {
-        return elementData.stream().filter(this::contains).toArray();
+        return new ArrayList<>(elementData)
+                .stream()
+                .filter(this::contains)
+                .toArray();
     }
 
     @Override
-    public <K> K[] toArray(K[] a) {
-        return (K[]) elementData.stream().filter(this::contains).collect(Collectors.toList()).toArray(a);
+    public <K> K[] toArray(final K[] a) {
+        return new ArrayList<>(elementData)
+                .stream()
+                .filter(this::contains)
+                .collect(Collectors.toList())
+                .toArray(a);
     }
 
     @Override
-    public synchronized boolean add(T t) {
-        elementData.add(t);
-        listOfDelete.add(false);
-        countList++;
-        return true;
-    }
-
-    @Override
-    public synchronized boolean remove(Object o) {
-        for (int i = 0; i < elementData.size(); i++) {
-            if (elementData.get(i).equals(o) && !listOfDelete.get(i)) {
-                listOfDelete.set(i, true);
-                countList--;
-                return true;
-            }
+    public boolean add(final T t) {
+        synchronized (sync) {
+            elementData.add(t);
+            listOfDelete.add(false);
+            countList.incrementAndGet();
+            return true;
         }
-        return false;
     }
 
     @Override
-    public boolean containsAll(Collection<?> c) {
-        for (Object element : c) {
-            if(!contains(element)){
+    public boolean remove(final Object o) {
+        synchronized (sync) {
+            int idx = elementData.indexOf(o);
+            if (idx < 0) return false;
+            listOfDelete.set(idx, true);
+            countList.decrementAndGet();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        for (final Object element : c) {
+            if (!contains(element)) {
                 return false;
             }
         }
@@ -96,42 +94,28 @@ public class AppendOnlyList<T> extends AbstractCollection<T>
     }
 
     @Override
-    public boolean addAll(Collection<? extends T> c) {
-        for (T t : c) {
+    public boolean addAll(final Collection<? extends T> c) {
+        for (final T t : c) {
             add(t);
         }
         return true;
     }
 
     @Override
-    public boolean removeAll(Collection<?> c) {
+    public boolean removeAll(final Collection<?> c) {
         boolean findElement = false;
-        for (Object element : c) {
-            if(remove(element))
+        for (final Object element : c) {
+            if (remove(element))
                 findElement = true;
         }
         return findElement;
     }
 
     @Override
-    public boolean retainAll(Collection<?> c) {
-        /*boolean findElement = false;
-        for(int i = 0; i < elementData.size(); i ++){
-            for(Object t : c) {
-                if(!contains(t))
-            }
-        }
-        for (Object t : c) {
-            if(!contains(t)){
-                remove(t);
-                findElement = true;
-            }
-        }
-        return findElement;*/
-
+    public boolean retainAll(final Collection<?> c) {
         Objects.requireNonNull(c);
         boolean findElement = false;
-        for (Object o : this) {
+        for (final Object o : this) {
             if (!c.contains(o)) {
                 remove(o);
                 findElement = true;
@@ -144,4 +128,44 @@ public class AppendOnlyList<T> extends AbstractCollection<T>
     public void clear() {
         removeAll(elementData);
     }
+
+
+    private final class Itr implements Iterator<T> {
+        private volatile int cursor = 0;
+        private int lastObj = -1;
+
+        Itr() {
+        }
+
+        @Override
+        public boolean hasNext() {
+            synchronized (sync) {
+                int size = elementData.size();
+                for (int n = cursor; n < size; n++) {
+                    if (listOfDelete.get(n)) continue;
+                    lastObj = n;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public T next() {
+            synchronized (sync) {
+                if (cursor >= elementData.size())
+                    throw new NoSuchElementException();
+                int i = cursor;
+                if (hasNext()) {
+                    cursor = lastObj;
+                }
+                if (listOfDelete.get(i)) {
+                    i = lastObj;
+                }
+                cursor++;
+                return elementData.get(i);
+            }
+        }
+    }
+
 }
